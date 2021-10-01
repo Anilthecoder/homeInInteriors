230e500e63b72d1f586d51dc92d955ca9f3f9e4e
Anil <anilgopisetti21@gmail.com>
2021-10-01 13:38:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { IndexComponent } from './components/index/index.component';
+import { ServiceComponent } from './components/service/service.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServiceComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'testimonial', component: TestimonialComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceComponent } from './components/service/service.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { IndexComponent } from './components/index/index.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    TestimonialComponent,
+    GalleryComponent,
+    ContactComponent,
+    AboutComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
